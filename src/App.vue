d9ef8c8aa9980b934728ce311661426f238fe19f
vermad843 <vishnuverma7524@gmail.com>
2019-12-19 11:10:14 +0530

    form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <div id="app" class = "container">
       <h1 class = "text-center">Todo App</h1>
+      <form>
+        <div class="form-group">
+          <label for="newTodo">New Todo</label>
+          <input
+             type="newTodo"
+             class="form-control"
+             id="newTodo"
+             aria-describedby="newTodoHelp"
+             placeholder="Enter Something"
+             >
+          <small
+             id="newTodoHelp"
+             class="form-text text-muted">
+             Enter a newTodo
+          </small>
+        </div>
+         <button
+           type = "submit"
+           class = "btn btn-primary"
+           >
+           Add Todo
+        </button>
+      </form>
   </div>
 </template>
-
 <script>
 export default {
   name: 'app',
