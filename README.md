570ea1e8dd8fd8c01da30e8500dfa116dbcb3410
vermad843 <vishnuverma7524@gmail.com>
2019-12-19 10:57:29 +0530

    Todo-App in the screen

---
+++ README.md
@@ -0,0 +1,15 @@
+Steps :Todo-App(Vue)
+
+
+[x] npm install -g vue-cli
+
+[x] vue init webpack-simple Todo-App
+
+[x] cd Todo-App
+
+[x] npm install
+
+[x] npm run dev
+
+[x]using bootswatch : https://bootswatch.com/4/minty/bootstrap.min.css
+
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class = "container">
+      <h1 class = "text-center">Todo App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
